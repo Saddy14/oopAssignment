0a9619dc456735f75b25cb161331215a96fdeb95
Saddy14 <1221301874@student.mmu.edu.my>
2023-05-03 12:26:31 +0800

    Deck Added Cards & Randomized the Deck also

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,33 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Main {
    public static void main(String[] args) {
-    
-        // Main
-        // System.out.println("Hello");
-        
 
+      String[] suit = { "c", "d", "h", "s" };
+      String[] rank = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "X", "J", "Q", "K" };
+
+      ArrayList<String> deck = new ArrayList<>();
+
+      for (int i = 0; i < 52; i++) {
+
+         deck.add(rank[i%13] + suit[i/13]);
+      }
+      // Check to see of deck is correctly working
+      System.out.println(deck);
+
+      // Random Deck
+      Random random = new Random();
+
+      for(int i = 0; i < 52; i++) {
+
+         int randomIndex = random.nextInt(52);
+         deck.set(i, deck.get(randomIndex));
+      }
+      
+      // Check to see of deck is correctly working
+      System.out.println();
+      System.out.println(deck);
    }
+
 }
