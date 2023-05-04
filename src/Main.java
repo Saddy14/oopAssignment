445a07a4faaf59f4d58842465433890320d215fc
Saddy14 <1221301874@student.mmu.edu.my>
2023-05-04 13:01:17 +0800

    Set Each Player Card that determine lead & more //

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,7 @@ import java.util.Random;
 
 public class Main {
    public static void main(String[] args) {
-
-             
+      
       Player player1 = new Player();
       Player player2 = new Player();
       Player player3 = new Player();
@@ -47,8 +46,35 @@ public class Main {
       System.out.println("After Suffle");
       System.out.println(deck);
       System.out.println("Deck Size: "+ deck.size());
+      System.out.println();
 
+      // Player 1 Cards that dertermines the 1st player
+      String[] rank1 = { "A", "5", "9","K" };
+      ArrayList<String> leadingP1Card = player1.setLeadPlayerCard(Player.suitForPlayer, rank1);
+      System.out.println("leadingP1Card: "+ leadingP1Card);
+      System.out.println("Size: "+ leadingP1Card.size());
+      System.out.println();
 
+      // Player 2 Cards that dertermines the 1st player
+      String[] rank2 = { "2", "6", "X"};
+      ArrayList<String> leadingP2Card = player2.setLeadPlayerCard(Player.suitForPlayer, rank2);
+      System.out.println("leadingP2Card: "+ leadingP2Card);
+      System.out.println("Size: "+ leadingP2Card.size());
+      System.out.println();
+
+      // Player 3 Cards that dertermines the 1st player
+      String[] rank3 = { "3", "7", "j"};
+      ArrayList<String> leadingP3Card = player3.setLeadPlayerCard(Player.suitForPlayer, rank3);
+      System.out.println("leadingP3Card: "+ leadingP3Card);
+      System.out.println("Size: "+ leadingP3Card.size());
+      System.out.println();
+
+      // Player 4 Cards that dertermines the 1st player
+      String[] rank4 = { "4", "8", "Q"};
+      ArrayList<String> leadingP4Card = player4.setLeadPlayerCard(Player.suitForPlayer, rank4);
+      System.out.println("leadingP1Card: "+ leadingP4Card);
+      System.out.println("Size: "+ leadingP4Card.size());
+      System.out.println();
 
       // Player 1 Cards
       player1.dealCard(deck);
@@ -56,22 +82,12 @@ public class Main {
       System.out.println("Player1: "+player1.playerCard);
       System.out.println("Deck Size: "+player1.playerCard.size()); 
       System.out.println();
-
-      // Player 1 
-      String[] suit1 = { "c", "d", "h", "s" };
-      String[] rank1 = { "A", "5", "9","K" };
-      ArrayList<String> leadingP1Card = player1.setLeadPlayerCard(suit1, rank1);
-      System.out.println("leadingP1Card: "+ leadingP1Card);
-      System.out.println("Size: "+ leadingP1Card.size());
-      System.out.println();
-
-      
       
       deck = removeFromDeck(deck, player1.playerCard); 
       System.out.println("New Deck\n"+deck);
       System.out.println("Deck Size: "+ deck.size());
 
-      // player2        // Check to see of deck is correctly working
+      // Player 2 Cards        
       player2.dealCard(deck);
       System.out.println();
       System.out.println("Player2: "+player2.playerCard);
@@ -82,7 +98,7 @@ public class Main {
       System.out.println("New Deck\n"+deck);
       System.out.println("Deck Size: "+ deck.size());
 
-      // player3        // Check to see of deck is correctly working
+      // Player 3 Cards
       player3.dealCard(deck);
       System.out.println();
       System.out.println("Player3: "+player3.playerCard);
@@ -93,7 +109,7 @@ public class Main {
       System.out.println("New Deck\n"+deck);
       System.out.println("Deck Size: "+ deck.size());
 
-      // player4        // Check to see of deck is correctly working
+      // Player 4 Cards
       player4.dealCard(deck);
       System.out.println();
       System.out.println("Player4: "+player4.playerCard);
@@ -106,8 +122,7 @@ public class Main {
 
    }
 
-
-
+   // Method that remove the card from the deck after 1 Player is given 7 Cards
    private static ArrayList<String> removeFromDeck (ArrayList<String> deck, ArrayList<String> playerCard) {
 
       for(int i = 0; i < playerCard.size(); i++) {
