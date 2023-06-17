9922ac98c5eb739f036d4f315dffe389f375da38
ASYRANI SYAZWAN BIN YUHANIS <93539057+AsyerIsMe@users.noreply.github.com>
2023-06-17 19:11:11 +0800

    Update Game.java
    Created a main menu for the game with a function to start a new game and load a saved game. Implemented serializable into Game.java so that the game can be saved. Made a new function that the user can save the game and continue the game. Also added an auto save when the user exited the game.

--- Part-2/Game.java
+++ Part-2/Game.java
@@ -1,16 +1,90 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.io.Serializable;
 
-public class Game {
+
+
+public class Game implements Serializable{
     
     private Deck deck1;
     private ArrayList<Player> playerList;
+    private ArrayList<String> deck;
+    private ArrayList<String> center;
+    private ArrayList<Integer> score;
+    private ArrayList<String> turn;
+    private SaveLoad save;
+
+
+    //Main Menu
+    public void mainMenu() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\033c");
+        System.out.println(" _______  ______  ");
+        System.out.println("|  ___  ||  __  | ");
+        System.out.println("| |  |__|| |  | | "); 
+        System.out.println("| |  ___ | |  | | ");
+        System.out.println("| | |_  || |  | | ");
+        System.out.println("| |   | || |  | | ");
+        System.out.println("| |___| || |__| | ");
+        System.out.println("|_______||______| ");
+        System.out.println(" _______  ______  ______  __     __ ");
+        System.out.println("|  ___  ||  __  ||  __  ||   \\  /  |");
+        System.out.println("| |   | || |  | || |  | ||    \\/   |");
+        System.out.println("| |___| || |  | || |  | || |\\   /| |");
+        System.out.println("|  ___ < | |  | || |  | || | \\_/ | |");
+        System.out.println("| |   | || |  | || |  | || |     | |");
+        System.out.println("| |___| || |__| || |__| || |     | |");
+        System.out.println("|_______||______||______||_|     |_|");
+        System.out.println("_______________________________");
+        System.out.println("|To start a new game, enter n |");
+        System.out.println("|To load a saved game, enter l|");
+        System.out.println("|_____________________________|");
+        System.out.println(" ");
+        System.out.println("        New Game(n)"); //start a new game
+        System.out.println("        Load Game(l)"); //load game file
+
+        System.out.print(">");
+
+        String game = scanner.nextLine();
+  
+        if (game.equals("n")) {   //New Game
+            System.out.print("\033c");
+            startGame();
+            
+        
+        }
+        else if (game.equals("l")) {   //Load Game
+            System.out.println("Loading game file...");
+            
+            
+        }
+        else {                                  //Wrong input
+            System.out.println("\033c");
+            System.out.println("Please input correctly");
+            System.out.println("_______________________________");
+            System.out.println("|To start a new game, enter n |");
+            System.out.println("|To load a saved game, enter l|");
+            System.out.println("_______________________________");
+            System.out.println(" ");
+            mainMenu();
+            
+            
+        }
+    }
 
     public void startGame() {
 
         deck1 = new Deck();
         deck1.generateDeck();
         deck1.makeDeckRandom();
+        save = new SaveLoad();
+        deck = new ArrayList<>();
+        center = new ArrayList<>();
+        score = new ArrayList<>();
+        turn = new ArrayList<>();
+        int deckSize;
+        int centerSize;
+        
 
         // Adding all the player into a Arraylist
         playerList = new ArrayList<>();
@@ -28,6 +102,8 @@ public class Game {
         // Add Card to Each Player
         Player.addCardToPlayer(playerList, deck1);
 
+        
+          
         Scanner scanner = new Scanner(System.in);
   
         int trick = 1;
@@ -36,14 +112,26 @@ public class Game {
         int winnerPlayer;
 
         // Print All stuff
+        System.out.println(" _______________________________");
+        System.out.println("|Game Functions:                |");
+        System.out.println("| d = Draw cards                |");
+        System.out.println("| s = Start a new game          |");
+        System.out.println("| v = Save the game             |");
+        System.out.println("| x = Exit and save the game    |");
+        System.out.println("|_______________________________|");
         gameStats(playerList, currentPlayerIndex, trick);
+        
+        
+            
+
         String userInput = scanner.nextLine();
-          
+
         while (!userInput.equals("x")) {
   
            Player currentPlayer = playerList.get(currentPlayerIndex);
  
             if (userInput.equals("s")) {
+                System.out.println("\033c");
                 startGame(); // start a new game
                 break;
             }
@@ -62,13 +150,50 @@ public class Game {
                         currentPlayer = playerList.get(currentPlayerIndex);
                         currentPlayer.addScore();
                     }
+                System.out.println("\033c");
                 }
                 else if(userInput.equals("d")) {
                     
                     deck1.dealCardFromDeck(currentPlayer); 
+                    System.out.println("\033c");
+
                 }
             }
             
+            else if (userInput.equals("v")) {
+                deckSize = deck1.getDeck().size();
+                centerSize = deck1.getCenter().size();
+                
+
+                save.SaveLoad1(playerList);
+                save.SaveLoad2(deck, deckSize);
+                save.SaveLoad3(center, centerSize);
+                save.SaveLoad4(score);
+                save.SaveLoad5(turn);
+
+                deck.add(deck1.getDeck().toString());
+                center.add(deck1.getCenter().toString());
+                turn.add(playerList.get(currentPlayerIndex).getName());
+                if (turn.size() > 1){
+                    turn.remove(0);
+                }
+                
+                //System.out.println(turn);
+                save.saveFile();
+                System.out.println("\033c");
+                System.out.println("Game Saved");
+                //saveload.LoadFile();
+                
+            } 
+            else{
+            System.out.println("\033c");
+            System.out.println("Please input correctly");
+            
+            } 
+                           
+
+            
+
             // gameStats(playerList, currentPlayerIndex, trick);
             // System.out.print(">");
             // userInput = scanner.nextLine();
@@ -92,14 +217,49 @@ public class Game {
                 else {
                   currentPlayerIndex = (currentPlayerIndex + 1) % playerList.size();
                 }
-
+            System.out.println("\033c");    
             }
+            System.out.println(" _______________________________");
+            System.out.println("|Game Functions:                |");
+            System.out.println("| d = Draw cards                |");
+            System.out.println("| s = Start a new game          |");
+            System.out.println("| v = Save the game             |");
+            System.out.println("| x = Exit and save the game    |");
+            System.out.println("|_______________________________|");
             gameStats(playerList, currentPlayerIndex, trick);
-            System.out.print(">");
+        
+
             userInput = scanner.nextLine();
             
         }
+
         scanner.close();
+
+        if (userInput.equals("x")){
+            deckSize = deck1.getDeck().size();
+            centerSize = deck1.getCenter().size();
+            
+            save.SaveLoad1(playerList);
+            save.SaveLoad2(deck, deckSize);
+            save.SaveLoad3(center, centerSize);
+            save.SaveLoad4(score);
+            save.SaveLoad5(turn);    
+            deck.add(deck1.getDeck().toString());
+            center.add(deck1.getCenter().toString());
+            turn.add(playerList.get(currentPlayerIndex).getName());
+            if (turn.size() > 1){
+                turn.remove(0);
+            }
+
+            //System.out.println(turn);
+            save.saveFile();
+            System.out.println("\033c");
+            System.out.println(" ________________________");
+            System.out.println("|       Game Saved       |");
+            System.out.println("|Thank you for playing!!!|");
+            System.out.println("|________________________|");
+        }
+        
     }
 
     
